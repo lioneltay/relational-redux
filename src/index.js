6a0c6eb3a4ffbdeb82e2d2f8b1287416ed869436
lionel tay <lionel tay>
2017-03-17 17:59:39 +1100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Root from 'Root'
+import 'styles/styles.scss'
+
+ReactDOM.render(
+	<Root />,
+	document.querySelector('#app')
+)
+
---
+++ webpack.config.babel.js
@@ -0,0 +1,51 @@
+import path from 'path'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import webpack from 'webpack'
+
+const settings = {
+  entry: {
+		bundle: './src/index.js'
+	},
+  resolve: {
+		alias: {
+			styles: path.join(__dirname, '/src/styles'),
+		},
+		modules: [path.resolve(__dirname, "src"), "node_modules"]
+  },
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+		publicPath: '/',
+	},
+	module: {
+		rules: [
+			{
+				use: 'babel-loader',
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+			},
+			{
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+				test: /\.scss$/,
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'src/index.html'
+		}),
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+		}),
+		new webpack.HotModuleReplacementPlugin(),
+	],
+	devServer: {
+		contentBase: path.resolve(__dirname, 'dist'),
+		historyApiFallback: true,
+		hot: true,
+		inline: true,
+	},
+}
+
+export default settings
